a1337a80b8abf6726a7d363d5f6afaf43c2a70a2
mangesh\mangeshlap <ozardekar.mangesh@gmail.com>
2020-07-31 06:50:31 +0530

    added packages structure

--- app/src/main/java/com/axxess/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/axxess/myapplication/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.axxess.myapplication
+package com.axxess.myapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.axxess.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
